37b5ebf4f7e02a7adf7be7eafa53c67d93e3002b
Bruna Campos <brunareginadecampos@gmail.com>
2025-03-03 21:55:15 -0300

    Projeto inicializado

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # o-jogo-da-velha
 Fazer um jogo da velha com 3 opções
+
+Primeira parte do jogo
+
+- Nesta primeira parte foi feita o inicio do projeto, colocando o include de locale,h e stdio.h para poder iniciar o codigo.
+- coloqueo o in main para o inicio do programa 
+- iniciei o cabecalho com printf e colocando o texto dentro 
+-utilizei o \n para quebra de linha
+- criei dois jogadores com o tipo char
+-criador a base do jogo
---
+++ velha.c
@@ -0,0 +1,56 @@
+#include <locale.h>
+#include <stdio.h>
+#include <string.h>
+
+
+
+int main() {
+    //permiti colocar acentos
+    setlocale(LC_ALL, "Portuguese");
+
+    // cabecalho do jogo
+    printf("\n\n");
+    printf("\n Bem vindo ao jogo da velha!!\n");
+    printf("\n\n");
+
+    //login do jogador
+    // dois jogadores e seus nomes
+    char nome_jogador_x[10];
+    char nome_jogador_o[10];
+    printf("Escreva o seu nome jogador X:  ");
+    printf("\n\n");
+    scanf("%s", &nome_jogador_x);
+    printf("\n\n");
+    printf("Escreva o seu nome jogador O:  ");
+    printf("\n\n");
+    scanf("%s", &nome_jogador_o);
+ 
+    printf("Nome jogador X %s\n", nome_jogador_x);
+    printf("Nome jogador O %s\n", nome_jogador_o);
+
+    printf("\n\n");
+    printf("Que o jogo comece!!");
+    printf("\n\n");
+
+    //estruturo do jogo da velha 
+    printf("             |             |             \n");
+    printf("    * * * *      |             |             \n");
+    printf("   *      *    |             |             \n");
+    printf("  *      *     |             |             \n");
+    printf("   * * * *       |             |             \n");
+    printf("_____________|_____________|_____________\n");
+    printf("             |             |             \n");
+    printf("             |             |             \n");
+    printf("             |             |             \n");
+    printf("             |             |             \n");
+    printf("             |             |             \n");
+    printf("_____________|_____________|_____________\n");
+    printf("             |             |             \n");
+    printf("             |             |             \n");
+    printf("             |             |             \n");
+    printf("             |             |             \n");
+    printf("             |             |             \n");
+   
+   
+
+}
